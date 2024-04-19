4a8e4c13682fca7451753242ca5d1deeea649abb
aurbina83 <aurbina83@gmail.com>
2024-04-19 11:19:52 -0400

    use plotly

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import json
-import os
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 from matplotlib import dates
 import streamlit as st
+import plotly.graph_objects as go
 
 # Set plot style
 plt.style.use("seaborn-v0_8-dark")
@@ -33,6 +32,8 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
+height = 800
 
 
 def scrape_data(ticker):
@@ -79,7 +80,7 @@ def compute_total_gex(spot, data):
 
 
 def compute_gex_by_strike(spot, data, ticker):
-    """Compute and plot GEX by strike"""
+    """Compute and plot GEX by strike using Plotly for interactivity"""
     # Compute total GEX by strike
     gex_by_strike = data.groupby("strike")["GEX"].sum() / 10**9
 
@@ -88,56 +89,86 @@ def compute_gex_by_strike(spot, data, ticker):
         gex_by_strike.index < spot * 1.15
     )
     limited_gex = gex_by_strike[limit_criteria]
+    tick_step = max(1, len(limited_gex) // 20)  # at most 15 labels on the x-axis
 
-    # Create a plot
-    fig, ax = plt.subplots()
-    ax.bar(
-        limited_gex.index,
-        limited_gex,
-        color="#FE53BB",
-        alpha=0.5,
+    # Create a plot using Plotly
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                x=limited_gex.index,
+                y=limited_gex,
+                text=limited_gex.round(2),
+                textposition="auto",
+                marker_color="#FE53BB",
+            )
+        ]
     )
-    ax.grid(color="#2A3459")
-    ax.set_xlabel("Strike", fontweight="light")
-    ax.set_ylabel("Gamma Exposure (Bn$)", fontweight="heavy")
-    ax.set_title(f"{ticker} GEX by Strike", fontweight="heavy")
 
-    # Set x-axis ticks and labels
-    # Display every 5th label
-    ticks = limited_gex.index[::5]
-    ax.set_xticks(ticks)
-    ax.set_xticklabels(ticks, rotation="vertical", fontsize=5)  # Adjust fontsize here
+    fig.update_layout(
+        title=f"{ticker} GEX by Strike",
+        xaxis_title="Strike",
+        yaxis_title="Gamma Exposure (Bn$)",
+        plot_bgcolor="#212946",
+        paper_bgcolor="#212946",
+        font=dict(color="#7FDBFF"),
+        xaxis=dict(
+            tickmode="linear",
+            tick0=limited_gex.index.min(),
+            dtick=tick_step,
+            tickangle=-45,
+        ),
+        height=height,
+    )
 
-    st.pyplot(fig)  # Streamlit function to display the figure
+    st.plotly_chart(fig, use_container_width=True)
 
 
 def compute_gex_by_expiration(data, ticker):
-    """Compute and plot GEX by expiration"""
+    """Compute and plot GEX by expiration using Plotly for interactivity"""
     # Limit data to one year
     selected_date = datetime.today() + timedelta(days=365)
     data = data[data["expiration"] < selected_date]
-
-    # Compute GEX by expiration date
     gex_by_expiration = data.groupby("expiration")["GEX"].sum() / 10**9
 
-    # Create a plot
-    fig, ax = plt.subplots()
-    ax.bar(
-        gex_by_expiration.index,
-        gex_by_expiration.values,
-        color="#FE53BB",
-        alpha=0.5,
+    fig = go.Figure(
+        data=[
+            go.Bar(
+                x=gex_by_expiration.index,
+                y=gex_by_expiration.values,
+                marker_color="#FE53BB",
+            )
+        ]
     )
-    ax.grid(color="#2A3459")
-    ax.set_xlabel("Expiration Date", fontweight="heavy")
-    ax.set_ylabel("Gamma Exposure (Bn$)", fontweight="heavy")
-    ax.set_title(f"{ticker} GEX by Expiration", fontweight="heavy")
-    plt.xticks(rotation=45)
-    st.pyplot(fig)  # Streamlit function to display the figure
+
+    # Adjusting margins and the positioning of the range slider
+    fig.update_layout(
+        title=f"{ticker} GEX by Expiration",
+        xaxis_title="Expiration Date",
+        yaxis_title="Gamma Exposure (Bn$)",
+        plot_bgcolor="#212946",
+        paper_bgcolor="#212946",
+        font=dict(color="#7FDBFF"),
+        height=height + 200,  # Set the height of the plot
+        hovermode="closest",  # Updates hover mode
+        margin=dict(t=60, l=50, r=50, b=100),  # Top, Left, Right, Bottom margins
+        xaxis=dict(
+            tickangle=-45,
+            tickformat="%b %d, %Y",
+            tickmode="auto",
+            nticks=20,  # Limits the number of ticks
+            rangeslider=dict(
+                visible=True,
+                thickness=0.05,  # Adjusts the thickness of the range slider
+            ),
+            domain=[0.05, 0.95],  # Leaves space at the bottom for the range slider
+        ),
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
 
 
 def print_gex_surface(spot, data, ticker):
-    """Plot 3D surface of GEX"""
+    """Plot 3D surface of GEX using Plotly with reversed strikes and axes swapped"""
     # Limit data to 1 year and +- 15% from ATM
     selected_date = datetime.today() + timedelta(days=365)
     limit_criteria = (
@@ -151,20 +182,63 @@ def print_gex_surface(spot, data, ticker):
     grouped_data = limited_data.groupby(["expiration", "strike"])["GEX"].sum() / 10**6
     grouped_data = grouped_data.reset_index()
 
-    # Create a 3D plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection="3d")
-    ax.plot_trisurf(
-        grouped_data["strike"],
-        dates.date2num(grouped_data["expiration"]),
-        grouped_data["GEX"],
-        cmap="seismic_r",
+    # Format and sort dates
+    grouped_data["expiration"] = grouped_data["expiration"].dt.strftime("%Y-%m-%d")
+    grouped_data.sort_values("expiration", inplace=True)
+
+    # Create grid values needed for 3D surface plot
+    x_unique = sorted(
+        grouped_data["expiration"].unique(), reverse=True
+    )  # Sorted unique expiration dates
+    y_unique = sorted(grouped_data["strike"].unique())  # Reversed sorted strikes
+    z = (
+        grouped_data.pivot(index="strike", columns="expiration", values="GEX")
+        .fillna(0)
+        .values
     )
-    ax.set_xlabel("Strike Price", fontweight="light")
-    ax.set_ylabel("Expiration Date", fontweight="light")
-    ax.set_zlabel("Gamma (M$)", fontweight="light")
-    ax.yaxis.set_major_formatter(dates.DateFormatter("%m/%d/%y"))
-    st.pyplot(fig)  # Streamlit function to display the figure
+
+    fig = go.Figure(
+        data=[
+            go.Surface(
+                z=z,
+                x=x_unique,
+                y=y_unique,
+                colorscale="Viridis",
+                cmin=z.min(),
+                cmax=z.max(),
+            )
+        ]
+    )
+
+    # Adjustments for the tick placement and formatting
+    x_ticks = max(1, len(x_unique) // 10)  # Show at most 10 x-axis labels
+    y_ticks = max(1, len(y_unique) // 10)  # Show at most 10 y-axis labels
+
+    fig.update_layout(
+        title=f"{ticker} Gamma Exposure Surface",
+        scene=dict(
+            xaxis_title="Expiration Date",
+            yaxis_title="Strike Price",
+            zaxis_title="Gamma (M$)",
+            xaxis=dict(
+                tickmode="array",
+                tickvals=x_unique[::x_ticks],
+                ticktext=[x for x in x_unique[::x_ticks]],
+            ),
+            yaxis=dict(
+                tickmode="array",
+                tickvals=y_unique[::y_ticks],
+                ticktext=[f"{int(y)}" for y in y_unique[::y_ticks]],
+            ),
+            zaxis=dict(tickmode="linear"),
+        ),
+        autosize=False,
+        width=height,
+        height=height - 100,
+        margin=dict(l=65, r=50, b=65, t=90),
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
 
 
 def main():
